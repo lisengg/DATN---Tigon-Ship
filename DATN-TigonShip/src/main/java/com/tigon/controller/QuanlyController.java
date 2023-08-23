49d65e809d24f7cd287715d4380fa3fa593515cc
tmco28 <tranmongco06@gmail.com>
2023-08-23 07:09:04 +0700

    23/08
    update giao dien admin

--- DATN-TigonShip/src/main/java/com/tigon/controller/QuanlyController.java
+++ DATN-TigonShip/src/main/java/com/tigon/controller/QuanlyController.java
@@ -7,9 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class QuanlyController {
-	@GetMapping("/quanly")
+	@GetMapping("/admin")
 	public String quanly(Model model) {
 		return "admin/index";
 	}
-
+	@GetMapping("/admin/tau")
+	public String tau(Model model) {
+		return "admin/tau";
+	}
 }
