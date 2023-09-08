2b8e1eb07a99c668b0df73500d0a4247b3ea9260
tmco28 <tranmongco06@gmail.com>
2023-09-08 20:24:02 +0700

    Revert "p2"
    This reverts commit 28450a02ebf6facf7d2f6e95fe4ad16f9e05f113.

---
+++ DATN-TigonShip/src/main/java/com/tigon/RestController/tauRestController.java
@@ -0,0 +1,57 @@
+package com.tigon.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tigon.dao.GheNgoiDAO;
+import com.tigon.dao.HangTauDAO;
+import com.tigon.dao.TauDAO;
+import com.tigon.model.Tau;
+
+@CrossOrigin("*")
+@RestController
+public class tauRestController {
+    @Autowired
+    TauDAO tauDAO;
+    @Autowired
+    HangTauDAO hangTauDAO;
+    @Autowired 
+    GheNgoiDAO gheNgoiDAO;
+    
+    @GetMapping("/rest/tau")
+    public Map<String, Object> getAll() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("tau", tauDAO.findAll());
+        map.put("hangtau", hangTauDAO.findAll());
+        map.put("ghengoi", gheNgoiDAO.findAll());
+        return map;
+    }
+    @GetMapping("/rest/tau/{id}")
+    public Tau getOne(@PathVariable("id") Integer id) {
+        return tauDAO.findById(id).get();
+    }
+  
+    @PostMapping("/rest/tau/save")
+    public Tau save(@RequestBody Tau tau) {
+        return tauDAO.save(tau);
+    }
+    @PutMapping("/rest/tau/{id}")
+    public Tau update(@PathVariable("id") Integer id, @RequestBody Tau tau) {
+        return tauDAO.save(tau);
+    }
+    @DeleteMapping("/rest/tau/{id}")
+    public void delete(@PathVariable("id") Integer id) {
+      tauDAO.deleteById(id);
+    }
+
+}
