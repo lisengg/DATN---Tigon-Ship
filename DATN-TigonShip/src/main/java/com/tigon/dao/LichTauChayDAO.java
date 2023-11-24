e46ff3caef35426f91322422c9641f43a6512e01
HoaLuc <tiethoaluc@gmail.com>
2023-11-24 21:10:52 +0700

    Chức năng tìm tàu và đặt vé

--- DATN-TigonShip/src/main/java/com/tigon/dao/LichTauChayDAO.java
+++ DATN-TigonShip/src/main/java/com/tigon/dao/LichTauChayDAO.java
@@ -3,11 +3,15 @@ package com.tigon.dao;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 
 import com.tigon.model.LichTauChay;
 
 public interface LichTauChayDAO extends JpaRepository<LichTauChay, Integer>{
-
+	@Query(value = "SELECT * FROM LICHTAUCHAY WHERE IDTUYEN = ?1", nativeQuery = true)
+	LichTauChay findByLichTau(Integer IDTUYEN);
+	
+	
 	
 }
---
+++ DATN-TigonShip/src/main/java/com/tigon/service/impl/LichTauServiceImpl.java
@@ -0,0 +1,32 @@
+package com.tigon.service.impl;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tigon.dao.LichTauChayDAO;
+import com.tigon.model.DatVe;
+import com.tigon.model.LichTauChay;
+import com.tigon.service.LichTauService;
+
+@Service
+public class LichTauServiceImpl implements LichTauService{
+
+	@Autowired
+	LichTauChayDAO ltdao;
+
+	@Override
+	public LichTauChay findByLichTau(int idtuyen) {
+		// TODO Auto-generated method stub
+		return ltdao.findByLichTau(idtuyen);
+	}
+
+	@Override
+	public LichTauChay findByid(Integer idlichtau) {
+		// TODO Auto-generated method stub
+		return ltdao.findById(idlichtau).get();
+	}
+	
+
+}
